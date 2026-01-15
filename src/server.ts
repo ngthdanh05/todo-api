530e967b746e721a65f870f01a6ee9e47d31d1df
ngthdanh <danhnt2444@gmail.com>
2026-01-15 23:26:19 +0700

    u

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://todos-ui-six.vercel.app"],
     credentials: true,
     exposedHeaders: ["Authorization"],
   })
